e19b05a023d12a4453e68d0209c0e3952e848696
Leodob1239 <125473930+Leodob1239@users.noreply.github.com>
2023-04-13 14:47:59 -0300

    Create register.js

---
+++ codigo/js/register.js
@@ -0,0 +1,12 @@
+function mostraSenha() {
+    var inputPass = document.getElementById('password')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if (inputPass.type === 'password') {
+        inputPass.setAttribute('type', 'text')
+        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+    } else {
+        inputPass.setAttribute('type', 'password')
+        btnShowPass.classList.replace('bi-eye-slash', 'bi-eye')
+    }
+}
